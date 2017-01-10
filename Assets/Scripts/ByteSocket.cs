fae5724da1bd5d392d360003f31dac71440fd704
BigBug <dj6680220@163.com>
2017-01-10 22:57:09 +0800

    Add newFile

--- Assets/Scripts/ByteSocket.cs
+++ Assets/Scripts/ByteSocket.cs
@@ -6,6 +6,8 @@ using System.IO;
 using System.Threading;
 using System.Text;
 using System.Net;
+using System;
+
 namespace Core.Net.Sockets
 {
     public class ByteSocket
@@ -46,6 +48,92 @@ namespace Core.Net.Sockets
         /// 包头消息体，一般用于解析消息Id,看需求
         /// </summary>
         byte[] m_RecvMsgHead = new byte[MSG_HEADER_LENGTH];
+        /// <summary>
+        /// 用于消息转换
+        /// </summary>
+        ByteArray m_ByteTemp = null;
+        /// <summary>
+        /// 包体内容
+        /// </summary>
+        byte[] m_MsgPackContent = null;
+        /// <summary>
+        /// 错误信息
+        /// </summary>
+        SocketError m_Error;
+        bool m_IsError = false;
+        bool m_IsConnected = false;
+
+        public ByteSocket()
+        {
+            EnterFrameManager.GetInstance().Register(EnterFrame);
+        }
+        void EnterFrame()
+        {
+
+        }
+        public void F_Connect(string ip, int port, bool isLogin = false, bool isForceQuit = false)
+        {
+            if (string.IsNullOrEmpty(ip)) return;
+            try
+            {
+                if (m_Socket == null)
+                {
+                    //缓存区下标重置
+                    m_StreamBuffer.Position = 0;
+                    IPAddress ipAddress = null;
+                    IPEndPoint ipEndPoint = null;
+                    if (IPAddress.TryParse(ip, out ipAddress))
+                    {
+                        ipEndPoint = new IPEndPoint(ipAddress, port);
+                        if (ipEndPoint.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            m_Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                        }
+                        else if (ipEndPoint.AddressFamily == AddressFamily.InterNetworkV6)
+                        {
+                            m_Socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            IPHostEntry ipHost = Dns.GetHostEntry(ip);
+                            IPAddress address = ipHost.AddressList[0];
+                            if (address.AddressFamily == AddressFamily.InterNetwork)
+                            {
+                                m_Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                            }
+                            else if (address.AddressFamily == AddressFamily.InterNetworkV6)
+                            {
+                                m_Socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
+                            }
+                            ipEndPoint = new IPEndPoint(address, port);
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.Log("IpError:" + e);
+                        }
+                    }
+                    m_Socket.BeginConnect(ipEndPoint,new AsyncCallback(ConnectCallBack), m_Socket);
+                }
+            }
+            catch (SocketException e)
+            {
+                m_IsError = true;
+                Debug.Log("SocketError:" + e);
+            }
+        }
+
+        void ConnectCallBack(IAsyncResult result)
+        {
+            try
+            {
+                Socket so = (Socket)result.AsyncState;
+            }
+            catch (SocketException e1)
+            { }
+        }
     }
 }
 
---
+++ Assets/Scripts/EnterFrameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+public class EnterFrameManager
+{
+    static EnterFrameManager instance;
+    public static EnterFrameManager GetInstance()
+    {
+        if (instance == null)
+            instance = new EnterFrameManager();
+        return instance;
+    }
+
+    public void Register(Action method)
+    {
+
+    }
+}
