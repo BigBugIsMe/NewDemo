4398e3db053347f8f327bf34816f699ff0f45146
BigBug <dj6680220@163.com>
2017-01-10 17:31:26 +0800

    change file

--- Assets/Scripts/NewDemo.cs
+++ Assets/Scripts/NewDemo.cs
@@ -2,14 +2,36 @@
 using System.Collections;
 
 public class NewDemo : MonoBehaviour {
-
-	// Use this for initialization
+    /// <summary>
+    /// 目标
+    /// </summary>
+    [SerializeField]
+    GameObject m_target;
+    /// <summary>
+    /// 位置
+    /// </summary>
+    [SerializeField]
+    Vector3 m_TargetPos;
+    float m_Velocity;
+    int V_ID;
+    // Use this for initialization
 	void Start () {
-	
+        Debug.Log("This is just a test script");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(20,20,50,30),"ok"))
+        {
+            
+        }
+    }
+    void OnRenderImage()
+    {
+        RenderTexture s = RenderTexture.GetTemporary(1280, 640, 0);
+    }
 }
